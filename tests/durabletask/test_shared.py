bdb1a444dec14717225231bd750c8c7fbd6acd41
Casper Nielsen <casper@diagrid.io>
2026-03-11 18:01:44 +0100

    chore(format): ruff
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

--- tests/durabletask/test_shared.py
+++ tests/durabletask/test_shared.py
@@ -11,7 +11,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-
 from durabletask.internal.shared import (
     DEFAULT_GRPC_KEEPALIVE_OPTIONS,
     _merge_grpc_options,
